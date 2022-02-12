a89b7d91c3785a758cba64e38d83dd6c45be7a19
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-02-12 14:16:00 +0500

    initial

---
+++ comparision-oper.php
@@ -0,0 +1,38 @@
+<?php
+
+// equal to operator
+
+$a = 20;
+$b = 20;
+echo $a == $b;
+echo "<br>";
+
+// equal to operator with datatype also equal
+
+$a = 20;
+$b = 20;
+echo $a === $b;
+echo "<br>";
+
+// not equal operator
+
+$a = 20;
+$b = 20;
+echo $a != $b;
+echo "<br>";
+
+// less than & Greater than operator
+
+$a = 20;
+$b = 5;
+echo $a < $b;
+echo $a > $b;
+echo "<br>";
+
+// not equal to wit datatype is also not equal
+
+$a = 20;
+$b = "5";
+echo $a !== $b;
+
+?>
