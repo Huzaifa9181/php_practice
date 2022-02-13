e69b39635b90482ef02bcd413ebb537d55999a28
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-02-13 15:08:24 +0500

    Switch Case

---
+++ switch_statement.php
@@ -0,0 +1,77 @@
+<?php
+
+$month = 5;
+
+switch($month){
+    case 1:
+        echo "January";
+        break;
+
+    case 2:
+        echo "Feburary";
+        break;
+
+    case 3:
+        echo "March";
+        break;
+
+    case 4:
+        echo "April";
+        break;
+
+    case 5:
+        echo "May";
+        break;
+
+    case 6:
+        echo "June";         
+        break;
+
+    case 7:
+        echo "July";
+        break;
+
+    case 8:
+        echo "August";
+        break;
+
+    case 9:
+        echo "September";
+        break;
+
+    case 10:
+        echo "October";
+        break;
+
+    case 11:
+        echo "November";       
+        break;
+
+    case 12:
+        echo "December";
+        break;
+    default :
+        echo "Enter a Valid Month Number";
+}
+
+// conditional switch case
+
+$age = 25;
+
+switch (true){
+case ($age >= 15 && $age <= 20):
+    echo "<br> you are eligible";
+    break;
+
+case ($age >= 21 && $age <= 30):
+    echo "<br>you are not eligible";
+    break;
+
+default:
+    echo "Enter a Valid Age";
+
+}
+
+
+?>
+
