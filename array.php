970ed500871a90f34e018ff09223ad3199b67ceb
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-03-04 01:06:08 +0500

    Update array.php

--- array.php
+++ array.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 $a = array(1,2,30);
 
 echo $a[2];
@@ -111,6 +112,8 @@ echo "</table>";
 
 echo "<br>";
 
+// multidimensional array
+
 $employes = [
     ["id" => 1,"name" => "huzaifa","designation" => "Owner","salary" => 50000],
     ["id" => 2,"name" => "hamza","designation" => "manager","salary" => 60000],
@@ -136,6 +139,159 @@ foreach($employes as list("id" => $id,"name" => $name, "designation" => $designa
   
 echo "</table>";
 
+
+// count te value of array for use this function [count] , [sizeof] , [array_count_values]
+
+$food = ['orange', 'apple','apple','Banana','pineapple'];
+
+echo count($food) . "<br>"; // 4
+echo sizeof($food); // 4
+echo ("<pre>");
+print_r(array_count_values($food));
+echo ("</pre>");
+
+echo "<br>";
+for($i = 0; $i < 4; $i++){
+    echo $food[$i] . "<br>";
+};
+
+
+$fruits = [
+    'fruit' =>  ['orange', 'apple','Banana','pineapple'],
+    'vegatable' =>  ['orange', 'apple','Banana','pineapple'],
+];
+
+echo count($fruits,1). "<br>";
+echo count($fruits['vegatable']);
+
+echo "<br>";
+
+$book = ['comp','phy','pst','sindhi'];
+
+echo in_array('phy', $book); // 0 or 1 answer
+
+echo "<br>";
+
+echo array_search('sindhi', $book); // index or key search
+
+if(in_array('phy', $book)){
+    echo "Succefully Find.";
+}else{
+    echo "Can't Find.";
+}
+echo "<br>";
+
+
+$copy = [
+    'computer' => 'comp',
+    'physics' => 'phy',
+    'pakistan_studies' => 'pst',
+    'sindhi' => 'sindhi'
+];
+
+    echo array_search('phy',$copy);
+    echo "<br>";
+
+    //  replaced function
+
+    $car = ["audi","mercedes","toyotta","KIA"];
+    $new_car = ["porshe","honda"];
+
+    $rep_car = array_replace($car,$new_car);
+
+    echo "<pre>";
+    print_r($rep_car);
+    echo "</pre>";
+
+
+    $color = [
+        "a" => ["red"],
+        "b" => ["Green" , "blue"]
+    ];
+
+    $new_color = [
+        "a" => ["pink"],
+        "b" => ["yellow"]
+    ];
+
+    $newArray = array_replace_recursive($color,$new_color);
+
+    echo "<pre>";
+    print_r($newArray);
+    echo "</pre>";  
+
+    // push or pop
+
+    $Newfruit = ["banana","apple","grapes"];
+
+    // array_pop($Newfruit);
+
+    array_push($Newfruit , "strawberries","lemons","oranges");
+
+    echo "<pre>";
+    print_r($Newfruit);
+    echo "</pre>";
+
+    // shift or unsift 
+
+    $laptop = ["dell","lenevo","hp"];
+
+    // array_shift($laptop);
+    array_unshift($laptop,"microsoft","my laptop");
+    
+    echo "<pre>";
+    print_r($laptop);
+    echo "</pre>";
+*/
+
+    // array merge
+
+    $food = [ 'a' => 'orange', 'b' => 'apple', 'c' => 'Banana', 'd' => 'pineapple'];
+
+    $veggie = [ 'a' => 'carrot', 'e' => 'pea'];
+
+    $newArray = array_merge($food,$veggie);
+
+
+    echo "<pre>";
+    print_r($newArray);
+    echo "</pre>";
+
+    // array combine
+
+    $name = ['huzaifa' , 'hamza' , 'sami'];
+    $age = [16 , 24 , 22];
+
+    $comb = array_combine($name,$age);
+
+    echo "<pre>";
+    print_r($comb);
+    echo "</pre>";
+
+    // array recursive
+ 
+    $car = ['a' => 'toyota', 'b' => 'audi', 'c' => 'kia'];
+    $model = ['a' => 2000, "e" => 1455 ];
+
+    $rec = array_merge_recursive($car,$model);
+
+    echo "<pre>";
+    print_r($rec);
+    echo "</pre>";
+
+    // type 2
+
+    $food = [ 'a' => 'orange', 'b' => 'apple', 'c' => 'Banana', 'd' => 'pineapple'];
+
+    $veggie = [ 'a' => [  'carrot',"banaan"],
+                 'e' => 'pea'];
+
+    $newArray = array_merge_recursive($food,$veggie);
+
+
+    echo "<pre>";
+    print_r($newArray);
+    echo "</pre>";
 
 
 ?>
